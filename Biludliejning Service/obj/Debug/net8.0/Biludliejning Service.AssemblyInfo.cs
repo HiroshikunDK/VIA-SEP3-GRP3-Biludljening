c31891c9bf2fd7f08363f281d45db57215d7c822
HiroshikunDK <36033304+HiroshikunDK@users.noreply.github.com>
2024-09-22 16:10:39 +0200

    add server projects

--- Biludliejning Service/obj/Debug/net8.0/Biludliejning Service.AssemblyInfo.cs
+++ Biludliejning Service/obj/Debug/net8.0/Biludliejning Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biludliejning Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02d542816fd243fa4544168167325879701924e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biludliejning Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biludliejning Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
