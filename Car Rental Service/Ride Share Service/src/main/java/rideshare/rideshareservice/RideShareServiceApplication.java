d600fc5435cd340da4b24a87b70f87e1a35fbed5
NotNebu <145534960+NotNebu@users.noreply.github.com>
2024-09-26 23:31:36 +0200

    Tilføjet alle microservices
    Oprettet som en monorepo

---
+++ Car Rental Service/Ride Share Service/src/main/java/rideshare/rideshareservice/RideShareServiceApplication.java
@@ -0,0 +1,13 @@
+package rideshare.rideshareservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideShareServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideShareServiceApplication.class, args);
+	}
+
+}
---
+++ Car Rental Service/Ride Share Service/src/test/java/rideshare/rideshareservice/RideShareServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rideshare.rideshareservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideShareServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
