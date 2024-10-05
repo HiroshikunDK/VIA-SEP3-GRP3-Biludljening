9879c633fd46c74ca9394de41db6316f6d07bc89
Nebula <145534960+NotNebu@users.noreply.github.com>
2024-10-05 15:16:08 +0200

    Changed from SHA256 to BCrypt

--- Car Rental Service/User Service/User Service/Utility/PasswordHelper.cs
+++ Car Rental Service/User Service/User Service/Utility/PasswordHelper.cs
@@ -9,22 +9,14 @@ namespace User_Service.Helpers
         // Hasher et plain text password med SHA256 -- Kan udskiftes til BCrypt, der er bedre
         public static string HashPassword(string password)
         {
-            using (var sha256 = SHA256.Create())
-            {
-                // Beregner hash metoden for password
-                var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
-                
-                // Konverterer hashen til en hexadecimal streng
-                return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
-            }
+            return BCrypt.Net.BCrypt.HashPassword(password);
         }
 
         // Verificere at det givne password matcher med den gemte hash
         public static bool VerifyPassword(string password, string passwordHash)
         {
             // Sammenligner input password med det gemte hash password
-            var hashedPassword = HashPassword(password);
-            return hashedPassword == passwordHash;
+            return BCrypt.Net.BCrypt.Verify(password, passwordHash);
         }
     }
 }
