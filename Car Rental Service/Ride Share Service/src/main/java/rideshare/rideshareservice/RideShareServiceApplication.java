8046e39fcea5db8c91a00ff500c2a8ee30b7c700
Nebula <145534960+NotNebu@users.noreply.github.com>
2024-10-05 15:17:49 +0200

    Typo

--- Car Rental Service/Ride Share Service/src/main/java/rideshare/rideshareservice/RideShareServiceApplication.java
+++ Car Rental Service/Ride Share Service/src/main/java/rideshare/rideshareservice/RideShareServiceApplication.java
@@ -14,24 +14,4 @@ public class RideShareServiceApplication {
 
 
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-public class PasswordHelper {
-
-	private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-	public static String hashPassword(String plainPassword) {
-		return encoder.encode(plainPassword);
-	}
-
-	public static boolean verifyPassword(String plainPassword, String hashedPassword) {
-		return encoder.matches(plainPassword, hashedPassword);
-	}
-}
-
-
-<dependency>
-    <groupId>org.springframework.security</groupId>
-    <artifactId>spring-security-core</artifactId>
-    <version>5.8.0</version>
-</dependency>
