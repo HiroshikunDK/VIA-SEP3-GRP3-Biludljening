de662839812f946b3a1f774ee1e595ab34ba1aa1
Emre <147695645+emre0247@users.noreply.github.com>
2024-10-31 10:15:47 +0100

    fejl version med databasen

--- Car Rental Service/Microservices/Car Management Service/src/main/Java/Model/Car.java
+++ Car Rental Service/Microservices/Car Management Service/src/main/Java/Model/Car.java
@@ -1,4 +1,113 @@
 package Model;
 
 public class Car {
-}
+   private String carId;
+    private String vin;
+    private int yearProduced;
+    private String manufacturer;
+    private String model;
+    private String color;
+    private String altName;
+    private int seats;
+    private int carRange;
+    private int locationHubRef;
+
+  public Car(String carId, String vin, int yearProduced, String manufacturer,
+      String model, String color, String altName, int seats, int carRange,
+      int locationHubRef)
+  {
+    this.carId = carId;
+    this.vin = vin;
+    this.yearProduced = yearProduced;
+    this.manufacturer = manufacturer;
+    this.model = model;
+    this.color = color;
+    this.altName = altName;
+    this.seats = seats;
+    this.carRange = carRange;
+    this.locationHubRef = locationHubRef;
+  }
+
+  // Getters and Setters
+    public String getCarId() {
+      return carId;
+    }
+
+    public void setCarId(String carId) {
+      this.carId = carId;
+    }
+
+    public String getVin() {
+      return vin;
+    }
+
+    public void setVin(String vin) {
+      this.vin = vin;
+    }
+
+    public int getYearProduced() {
+      return yearProduced;
+    }
+
+    public void setYearProduced(int yearProduced) {
+      this.yearProduced = yearProduced;
+    }
+
+    public String getManufacturer() {
+      return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+      this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+      return model;
+    }
+
+    public void setModel(String model) {
+      this.model = model;
+    }
+
+    public String getColor() {
+      return color;
+    }
+
+    public void setColor(String color) {
+      this.color = color;
+    }
+
+    public String getAltName() {
+      return altName;
+    }
+
+    public void setAltName(String altName) {
+      this.altName = altName;
+    }
+
+    public int getSeats() {
+      return seats;
+    }
+
+    public void setSeats(int seats) {
+      this.seats = seats;
+    }
+
+    public int getCarRange() {
+      return carRange;
+    }
+
+    public void setCarRange(int carRange) {
+      this.carRange = carRange;
+    }
+
+    public int getLocationHubRef() {
+      return locationHubRef;
+    }
+
+    public void setLocationHubRef(int locationHubRef) {
+      this.locationHubRef = locationHubRef;
+    }
+  }
+
+
--- Car Rental Service/Microservices/Car Management Service/src/main/Java/Service/CarService.java
+++ Car Rental Service/Microservices/Car Management Service/src/main/Java/Service/CarService.java
@@ -1,4 +1,39 @@
 package Service;
 
-public class CarService {
+import CarManagementService.grpc.CarManagement;
+import CarManagementService.grpc.CarServiceGrpc;
+import Persistence.DataBaseHelper;
+import io.grpc.stub.StreamObserver;
+import Model.Car;
+
+public class CarService extends CarServiceGrpc.CarServiceImplBase
+{
+
+  private final DataBaseHelper dataBaseHelper;
+
+  public CarService(DataBaseHelper dataBaseHelper)
+  {
+    this.dataBaseHelper = dataBaseHelper;
+  }
+
+  @Override public void addCar(CarManagement.Car request, StreamObserver<CarManagement.CarResponse> responseObserver)
+  {
+    System.out.println(request.getVin());
+    System.out.println(request.getCarId());
+    // Make a new car object
+    Car car = new Car(request.getCarId(), request.getVin(), request.getYearproduced(),
+        request.getManufactor(), request.getModel(), request.getColor(), request.getAltname(),
+        request.getSeats(), request.getCarrange(), request.getLocationhubref());
+
+    dataBaseHelper.registerCar(car);
+    responseObserver.onNext(CarManagement.CarResponse.newBuilder().build());
+    responseObserver.onCompleted();
+  }
+
+  public void getAllCars(StreamObserver<CarManagement.CarResponse> responseObserver)
+  {
+
+  }
+
+
 }
