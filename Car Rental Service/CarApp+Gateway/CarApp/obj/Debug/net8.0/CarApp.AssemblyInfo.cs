7d34e4ac3d015a64e0941791c41a3bd69a76787b
HiroshikunDK <36033304+HiroshikunDK@users.noreply.github.com>
2024-11-03 17:46:58 +0100

    Force shared folder to Correct version

--- Car Rental Service/CarApp+Gateway/CarApp/obj/Debug/net8.0/CarApp.AssemblyInfo.cs
+++ Car Rental Service/CarApp+Gateway/CarApp/obj/Debug/net8.0/CarApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9481a3aa0ba568638d5939ca5777128b9ce9ae9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+914b3fe9ff8b0cbed97ce57226a7f87d86f7c2f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Car Rental Service/CarApp+Gateway/gRPCGateway/obj/Debug/net8.0/gRPCGateway.AssemblyInfo.cs
+++ Car Rental Service/CarApp+Gateway/gRPCGateway/obj/Debug/net8.0/gRPCGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gRPCGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98341e8474ffb62bbdd8d43d396cfab965040317")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+914b3fe9ff8b0cbed97ce57226a7f87d86f7c2f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("gRPCGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gRPCGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
