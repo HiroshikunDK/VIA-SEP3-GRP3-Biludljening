2993b9ab2c0e533abbf96f70c2bc4ebea81842f8
adresf1 <147700879+adresf1@users.noreply.github.com>
2024-11-03 23:07:07 +0100

    Update UserService.java

--- Car Rental Service/Microservices/User Service/src/main/Java/Service/UserService.java
+++ Car Rental Service/Microservices/User Service/src/main/Java/Service/UserService.java
@@ -39,23 +39,62 @@ public class UserService extends UserServiceGrpc.UserServiceImplBase {
         responseObserver.onCompleted();
     }
 
-    @Override
+  @Override
     public void loginUser(LoginRequest request, StreamObserver<LoginResponse> responseObserver) {
+        Optional<User> userOptional = userRepository.getUserByUsername(request.getUsername());
+        LoginResponse response;
+        if (userOptional.isPresent() && userOptional.get().getPassword().equals(request.getPassword())) {
+            response = LoginResponse.newBuilder()
+                    .setToken("dummy-token-for-" + request.getUsername())
+                    .build();
+        } else {
+            response = LoginResponse.newBuilder().setToken("").build();
+        }
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
       
     }
 
     @Override
     public void getUserById(UserRequest request, StreamObserver<UserResponse> responseObserver) {
+        Optional<User> userOptional = userRepository.getUserById(request.getId());
+        UserResponse response = userOptional.map(user -> UserResponse.newBuilder()
+                        .setSuccess(true)
+                        .setMessage("User found")
+                        .setUser(convertToProtoUser(user))
+                        .build())
+                .orElse(UserResponse.newBuilder()
+                        .setSuccess(false)
+                        .setMessage("User not found")
+                        .build());
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
        
     }
 
     @Override
     public void updateUser(UserOuterClass.User request, StreamObserver<UserResponse> responseObserver) {
+        User updatedUser = new User(request.getId(), request.getUsername(), request.getEmail(), request.getRole(), request.getPassword());
+        userRepository.updateUser(updatedUser);
+        UserResponse response = UserResponse.newBuilder()
+                .setSuccess(true)
+                .setMessage("User updated successfully")
+                .setUser(convertToProtoUser(updatedUser))
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
       
     }
 
     @Override
     public void deleteUser(UserRequest request, StreamObserver<UserResponse> responseObserver) {
+        boolean success = userRepository.deleteUser(request.getId());
+        UserResponse response = UserResponse.newBuilder()
+                .setSuccess(success)
+                .setMessage(success ? "User deleted successfully" : "User not found")
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
        
     }
 
