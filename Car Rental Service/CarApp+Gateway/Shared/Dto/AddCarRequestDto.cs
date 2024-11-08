4d4b3717a323763db45cb2ddd740e56f677f532b
Nebula <145534960+NotNebu@users.noreply.github.com>
2024-11-08 10:27:16 +0100

    Restructure

--- Car Rental Service/CarApp+Gateway/Shared/Dto/AddCarRequestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Shared.Dto;
-
-public class AddCarRequestDto
-{
-    public int CarId { get; set; }
-    public string Model { get; set; }
-    public string Color { get; set; }
-    public int Seats { get; set; }
-}
--- Car Rental Service/CarApp+Gateway/Shared/Dto/RegisterUserRequestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Shared.Dto;
-
-public class RegisterUserRequestDto
-{
-    public string Username { get; set; }
-    public string Email { get; set; }
-    public string Role { get; set; }
-    public string Password { get; set; }
-}
