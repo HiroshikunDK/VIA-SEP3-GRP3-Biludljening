389727a3d1fc728dc42eada12798c3fb4910a2e1
Nebula <145534960+NotNebu@users.noreply.github.com>
2024-11-08 10:27:44 +0100

    Restructure

--- Car Rental Service/CarApp+Gateway/Shared/Dto/CreateRideshareRequestDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Shared.Dto;
-
-public class CreateRideshareRequestDto
-{
-    public int CarId { get; set; }
-    public string DriverUsername { get; set; }
-    public int AvailableSeats { get; set; }
-}
